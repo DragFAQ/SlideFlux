663447d194a5cc15c66b101f83ba972dff88cf71
Sergii Safonov <Sergii.Safonov@3shape.com>
2017-07-28 14:30:52 +0300

    Initial commit

---
+++ app/src/main/java/com/drag/slideflux/InputQRActivity.java
@@ -0,0 +1,13 @@
+package com.drag.slideflux;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InputQRActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_qr);
+    }
+}
---
+++ app/src/main/java/com/drag/slideflux/netHelper.java
@@ -0,0 +1,20 @@
+package com.drag.slideflux;
+
+import java.io.IOException;
+
+public class netHelper {
+    public static final String SERVER = "http://www.google.com";
+
+    public static String request (String str) throws IOException {
+/*        URL url = new URL(SERVER);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } finally {
+            urlConnection.disconnect();
+        }*/
+
+        return "";
+    }
+}
